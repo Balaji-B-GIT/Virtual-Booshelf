29e0ae4ea14a2d321e656381b5abe3549ac61ef8
Balaji B <balaji.balachandar1@gmail.com>
2024-11-19 23:45:08 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from enum import UNIQUE
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -16,7 +20,26 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY-DATABASE-URL'] = "sqlite:///books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 all_books = []
+
+class Book(db.Model):
+    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    title:Mapped[str] = mapped_column(String(250), nullable=False,unique=True)
+    author:Mapped[str] = mapped_column(String(250),nullable=False)
+    rating:Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/', methods=["POST","GET"])
