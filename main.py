ba7b3f1fa7337769d954871da8297b411d525df5
Balaji B <balaji.balachandar1@gmail.com>
2024-11-20 17:14:30 +0530

    Added 'Edit Rating' and 'Delete' feature.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from enum import UNIQUE
-
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -23,11 +21,11 @@ app = Flask(__name__)
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY-DATABASE-URL'] = "sqlite:///books-collection.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-collection.db"
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-all_books = []
+
 
 class Book(db.Model):
     id:Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -36,28 +34,54 @@ class Book(db.Model):
     rating:Mapped[float] = mapped_column(Float, nullable=False)
 
     def __repr__(self):
-        return f'<Book {self.title}>'
+        return f'<{self.id}>'
 
 with app.app_context():
     db.create_all()
 
 
-@app.route('/', methods=["POST","GET"])
+@app.route('/')
 def home():
+    # Read all DB Data
+    all_books = db.session.execute(db.select(Book).order_by(Book.title)).scalars().all()
+    return render_template("index.html", books = all_books)
+
+
+@app.route("/add", methods=["POST","GET"])
+def add():
     if request.method == "POST":
         data = request.form
-        dicti = {
-            'title':data["name"],
-            'author':data["author"],
-            'rating':data["rating"],
-        }
-        all_books.append(dicti)
-    return render_template("index.html",books = all_books)
+        title = data["name"]
+        author = data["author"]
+        rating = data["rating"]
 
-
-@app.route("/add")
-def add():
+        # Add record
+        new_book = Book(title = title,author = author, rating = rating)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template("add.html")
+
+@app.route("/edit/<int:id>", methods=["POST","GET"])
+def edit(id):
+    book_selected = Book.query.get(id)
+    if request.method == "POST":
+        book_selected.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html",book = book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    # DELETE A RECORD BY ID
+    book_to_delete = Book.query.get(book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
